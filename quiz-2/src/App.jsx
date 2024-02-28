75546c810caaf89dbf3cce6bcfa4023771c4fe6b
jahnavreddy <jahnavredy12@gmail.com>
2024-02-28 16:16:26 +0530

    projectDone

--- quiz-2/src/App.jsx
+++ quiz-2/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Quiz from './components/Quiz';
 
 
 function App() {
   return (
     <div className="App">
+      <Quiz />
  
     </div>
   );
--- quiz-2/src/components/Quiz.jsx
+++ quiz-2/src/components/Quiz.jsx
@@ -1,65 +1,47 @@
-import React, { useCallback, useState } from 'react';
-import questions from './../../../resources/quizQuestion.json';
-import Home from './Home';
+import React, { useState } from 'react';
+import quizQuestions from "../../../resources/quizQuestion.json"
+import "../App.css"
 
-function Quiz() {
-  const [q_no, setQ_no] = useState(0);
-  const [isHome, setIsHome] = useState(false);
+export default function Quiz() {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
 
-  const handleQuit = useCallback(() => {
-    setIsHome(true);
-  }, []);
+    const question = quizQuestions[currentQuestion];
 
-  const handleNext = useCallback(() => {
-    if (q_no < questions.length - 1) {
-      setQ_no(q_no + 1);
-    }
-  }, [q_no]);
+    const prevQuestion = () => {
+        setCurrentQuestion(prev => Math.max(prev - 1, 0));
+    };
 
-  const handlePrevious = useCallback(() => {
-    if (q_no > 0) {
-      setQ_no(q_no - 1);
-    }
-  }, [q_no]);
+    const nextQuestion = () => {
+        setCurrentQuestion(prev => Math.min(prev + 1, quizQuestions.length - 1));
+    };
 
-  const currentQuestion = questions[q_no];
+    const reset = () => {
+        let resp = window.confirm("Are you sure you want to quit?");
+        if (resp) {
+            setCurrentQuestion(0);
+        }
+    };
 
-  if (isHome) {
-
-    return <Home />;
-  }
-
-  return (
-    <>
-      <h1>Question</h1>
-      <h4>
-        {q_no + 1} of {questions.length}
-      </h4>
-      <h2>{currentQuestion.question}</h2>
-      <div>
-        <div className="buttons-top">
-          <button className="button-1">{currentQuestion.optionA}</button>
-          <button className="button-2">{currentQuestion.optionB}</button>
+    return (
+        <div className='main'>
+            <h2 className='title'>Question</h2>
+            <h5>{currentQuestion + 1} of {quizQuestions.length}</h5>
+            <p className='question'>{question.question}</p>
+            <div className='container'>
+                <div className='group'>
+                    <button className='option'>{question.optionA}</button>
+                    <button className='option'>{question.optionB}</button>
+                </div>
+                <div className='group'>
+                    <button className='option'>{question.optionC}</button>
+                    <button className='option'>{question.optionD}</button>
+                </div>
+            </div>
+            <div className="buttons">
+                <button id='previous' onClick={prevQuestion} disabled={currentQuestion === 0}>Previous</button>
+                <button id='next' onClick={nextQuestion} disabled={currentQuestion === quizQuestions.length - 1}>Next</button>
+                <button id='quit' onClick={reset}>Quit</button>
+            </div>
         </div>
-        <div className="buttons-bottom">
-          <button className="button-3">{currentQuestion.optionC}</button>
-          <button className="button-4">{currentQuestion.optionD}</button>
-        </div>
-      </div>
-
-      <div className="options">
-        <button className="previous" onClick={handlePrevious}>
-          Previous
-        </button>
-        <button className="next" onClick={handleNext}>
-          Next
-        </button>
-        <button className="quit" onClick={handleQuit}>
-          Quit
-        </button>
-      </div>
-    </>
-  );
+    );
 }
-
-export default Quiz;
